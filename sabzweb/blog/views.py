66b60eb4d1da4301c2d46da0e634667442e51bce
alikiansaie <ali.kian.saii@gmail.com>
2024-05-20 20:51:32 +0330

    Debuging EmptyPage and PageNotInteger

--- sabzweb/blog/views.py
+++ sabzweb/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse, Http404
 from . models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage , PageNotAnInteger
 # Create your views here.
 
 
@@ -13,6 +13,12 @@ def post_list(request):
     posts = Post.published.all()
     paginator = Paginator(posts,2)
     page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(paginator.num_pages)
     posts = paginator.page(page_number)
     context = {
         'posts': posts
