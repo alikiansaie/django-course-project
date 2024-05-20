e37bf7cb4cf01dace8c58b3f853f33718fe41cc0
alikiansaie <ali.kian.saii@gmail.com>
2024-05-20 17:14:39 +0330

    adding pagination in view file

--- sabzweb/blog/views.py
+++ sabzweb/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse, Http404
 from . models import Post
+from django.core.paginator import Paginator
 # Create your views here.
 
 
@@ -10,6 +11,9 @@ def index(request):
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts,2)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     context = {
         'posts': posts
     }
